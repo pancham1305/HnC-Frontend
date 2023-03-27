b4f66a2048c4db68e4281a49ae4ce2321a25a8cc
Pancham Mehta <pancham1305@gmail.com>
2023-03-27 13:40:03 +0530

    Fixed Dropdown

--- public/js/services.js
+++ public/js/services.js
@@ -4,28 +4,21 @@ function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  }
+const cityName = document.querySelector(".cityName");
+const links = document.querySelectorAll(".links");
+for (let i of links) {
+  i.addEventListener("click", (e) => {
+    e.preventDefault();
+    const text = e.target.innerText.toUpperCase();
+    cityName.innerText = text;
+  });
 }
-
+  
 // Filter Box JS
 
 var button = document.getElementById("filter-button");
 var container = document.getElementById("filter-container");
 var input = document.querySelectorAll("input");
-
 
 button.onclick = function (e) {
   e.stopPropagation();
