b46df4a481470d6e16f9120ad8a2a21ebfc1a81c
Pancham Mehta <pancham1305@gmail.com>
2023-03-28 12:46:36 +0530

    Delete temp.js

--- temp.js
+++
@@ -1,7 +0,0 @@
-fetch("http://localhost:50000/api/userLoc", {
-  method: "post",
-  body: JSON.stringify({ hi: 1 }),
-  headers: {
-    "Content-Type": "application/json",
-  },
-}).then((e) => console.log(e));
