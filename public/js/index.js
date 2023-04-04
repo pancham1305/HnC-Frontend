e35c64db2ec7605ed708b6d7fdee90d27b4f53ac
Ayaka <user.satoshi69@gmail.com>
2023-04-04 12:58:18 +0530

    updates

--- public/js/index.js
+++ public/js/index.js
@@ -2,30 +2,59 @@
 toggle between hiding and showing the dropdown content */
 let isDropdownToggled = false;
 document.addEventListener( 'DOMContentLoaded', () =>
-{ 
+{
   const drop = document.getElementById( "dropbtn" );
-  drop.innerHTML = `${cities.map(city => `<option class="links">${city}</option>`).join("\n")}`
-})
+  drop.innerHTML = `${ cities.map( city => `<option class="links">${ city.name }</option>` ).join( "\n" ) }`;
+  if ( navigator.geolocation )
+  {
+    let loc;
+    navigator.geolocation.getCurrentPosition( async ( pos ) =>
+    {
+      const { latitude, longitude } = pos.coords;
+      const exactloc = await fetch(
+        `https://HnC-Backend.pancham1305.repl.co/api/userLoc`,
+        {
+          method: "POST",
+          body: JSON.stringify( { lat:latitude, long:longitude } ),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      ).then( ( r ) => r.json() );
+       console.log(exactloc);
+      loc = exactloc.features[ 0 ].properties.county;
+      console.log( loc );
+      let index = cities.findIndex( x => x.name === loc || x.name === exactloc.features[ 0 ].properties.state_district );
+      console.log( index );
+        
+      drop.selectedIndex = index;
+    }, ( err ) =>
+    {
+      console.error( err );
+    }
+    );
+  }
+} );
 
-const cityName = document.querySelector(".cityName");
-const links = document.querySelectorAll(".links");
-for (let i of links) {
-  i.addEventListener("click", (e) => {
-    e.preventDefault();
-    const text = e.target.innerText.toUpperCase();
-    cityName.innerText = text;
-  });
-}
+
+
 const searchBar = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 
-btn.addEventListener("click", async (e) => {
-  e.preventDefault();
-  const info = searchBar.value;
-  const name = cityName.innerText;
-  const query = searchBar.value;
-  console.log(name, query);
-  localStorage.setItem("searchinfo", JSON.stringify({ name, query }));
-  window.location.href = "./html/services.html";
+btn.addEventListener( "click", async ( e ) =>
+{
+  const drop = document.getElementById( "dropbtn" );
+// check if user has disabled location access
+
+e.preventDefault();
+const info = searchBar.value;
+const name = drop.options[drop.selectedIndex].text;
+const query = searchBar.value;
+console.log(name, query);
+localStorage.setItem(
+    "searchinfo",
+    JSON.stringify({ name, query, selectedIndex: drop.selectedIndex }),
+);
+window.location.href = "./html/services.html";
 });
 
