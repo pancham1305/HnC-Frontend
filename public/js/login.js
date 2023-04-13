1f000a076ee47f1e726177f5006d2a43221261d3
Ayaka <user.satoshi69@gmail.com>
2023-04-13 14:48:14 +0530

    link updated

--- public/js/login.js
+++ public/js/login.js
@@ -1,128 +1,135 @@
 const loader = document.querySelector(".loader");
 document.addEventListener("DOMContentLoaded", () => {
-  const login = document.getElementById("login");
-  const error = document.getElementById("error");
-  error.style.height = "0px";
-  error.style.top = "-30px";
+    const login = document.getElementById("login");
+    const error = document.getElementById("error");
+    error.style.height = "0px";
+    error.style.top = "-30px";
 
-  const password = document.getElementById("pass");
-  let p = password?.value;
+    const password = document.getElementById("pass");
+    let p = password?.value;
 
-  const updatepass = () => {
-    password.type = "text";
-    // only save the part that was selected
-    const x = p.slice(password.selectionStart, password.selectionEnd);
-    password.value = p
-      .split("")
-      .map((y, i) =>
-        i >= password.selectionStart && i < password.selectionEnd ? y : "*"
-      )
-      .join("");
-    // keep the part selected after the update
-    password.focus();
-    password.setSelectionRange(password.selectionStart, password.selectionEnd);
-  };
-
-  const restorePass = () => {
-    console.log(p);
-    password.type = "password";
-    password.value = p;
-  };
-
-  password?.addEventListener("change", (e) => (p = e.target.value));
-  password?.addEventListener("keyup", (e) => (p = e.target.value));
-  password?.addEventListener("select", (e) => updatepass());
-  password.onblur = password.onfocus = password.onmousedown = restorePass;
-
-  const closebtn = document.getElementById("closeerrorbtn");
-  console.log({ closebtn });
-
-  login.addEventListener("click", async (e) => {
-    try {
-      e.preventDefault();
-      let phone = document.getElementById("email").value;
-      const password = document.getElementById("pass").value;
-      if (!phone || !password) return sendError("Please fill all the fields");
-      if (phone.length === 13) phone = phone.slice(3);
-
-      phone = Number(phone).toString();
-      if (isNaN(phone) || phone.length !== 10)
-        return sendError("Invalid Phone Number");
-      if (password.length < 8)
-        return sendError("Password must be atleast 8 characters long");
-
-      const data = {
-        phone,
-        password,
-        token: localStorage.getItem("uid"),
-        endpoint: "/api/login",
-      };
-      const temp = login.innerHTML;
-      login.innerHTML = `<div class="spinner"></div>`;
-      const resData = await fetch("https://pancham1305-proxy.deno.dev/", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-      }).then((d) => d.json());
-      if (resData.status === 200) {
-        localStorage.setItem("user", JSON.stringify(resData.data));
-        localStorage.setItem("token", resData.token);
-        window.location.href = "./services.html";
-      } else {
-        login.innerHTML = temp;
-        sendError(resData.message);
-      }
-    } catch (e) {
-      login.innerHTML = temp;
-      sendError(`Failed to login: ${e.message}`);
-    }
-  });
-
-  const guestbtn = document.getElementById("guest");
-  guestbtn.addEventListener("click", async (e) => {
-    const temp = guestbtn.innerHTML;
-    guestbtn.innerHTML = `<div class="spinner"></div>
-`;
-    const data = {
-      phone: "+910000000000",
-      password: "guestOogwayrocks",
-      uid: "0".repeat(64),
-      endpoint: "/api/login",
+    const updatepass = () => {
+        password.type = "text";
+        // only save the part that was selected
+        const x = p.slice(password.selectionStart, password.selectionEnd);
+        password.value = p
+            .split("")
+            .map((y, i) =>
+                i >= password.selectionStart && i < password.selectionEnd
+                    ? y
+                    : "*",
+            )
+            .join("");
+        // keep the part selected after the update
+        password.focus();
+        password.setSelectionRange(
+            password.selectionStart,
+            password.selectionEnd,
+        );
     };
 
-    const res = await fetch("https://pancham1305-proxy.deno.dev/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then((d) => d.json());
-    if (res.status === 200) {
-      localStorage.setItem("user", JSON.stringify(res.data));
-      window.location.href = "./services.html";
-    } else {
-      guestbtn.innerHTML = temp;
-      const error = document.getElementById("error");
-      error.style.height = "";
-      error.style.top = "";
-      error.innerHTML += `<div class='etext'>${res.message}</div>`;
-    }
-  });
+    const restorePass = () => {
+        console.log(p);
+        password.type = "password";
+        password.value = p;
+    };
+
+    password?.addEventListener("change", (e) => (p = e.target.value));
+    password?.addEventListener("keyup", (e) => (p = e.target.value));
+    password?.addEventListener("select", (e) => updatepass());
+    password.onblur = password.onfocus = password.onmousedown = restorePass;
+
+    const closebtn = document.getElementById("closeerrorbtn");
+    console.log({ closebtn });
+
+    login.addEventListener("click", async (e) => {
+        try {
+            e.preventDefault();
+            let phone = document.getElementById("email").value;
+            const password = document.getElementById("pass").value;
+            if (!phone || !password)
+                return sendError("Please fill all the fields");
+            if (phone.length === 13) phone = phone.slice(3);
+
+            phone = Number(phone).toString();
+            if (isNaN(phone) || phone.length !== 10)
+                return sendError("Invalid Phone Number");
+            if (password.length < 8)
+                return sendError("Password must be atleast 8 characters long");
+
+            const data = {
+                phone,
+                password,
+                token: localStorage.getItem("uid"),
+                endpoint: "/api/login",
+            };
+            const temp = login.innerHTML;
+            login.innerHTML = `<div class="spinner"></div>`;
+            const resData = await fetch("https://pancham1305-proxy.deno.dev/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            }).then((d) => d.json());
+            if (resData.status === 200) {
+                localStorage.setItem("user", JSON.stringify(resData.data));
+                localStorage.setItem("token", resData.token);
+                window.location.href = "./services.html";
+            } else {
+                login.innerHTML = temp;
+                sendError(resData.message);
+            }
+        } catch (e) {
+            login.innerHTML = temp;
+            sendError(`Failed to login: ${e.message}`);
+        }
+    });
+
+    const guestbtn = document.getElementById("guest");
+    guestbtn.addEventListener("click", async (e) => {
+        const temp = guestbtn.innerHTML;
+        guestbtn.innerHTML = `<div class="spinner"></div>
+`;
+        const data = {
+            phone: "+910000000000",
+            password: "guestOogwayrocks",
+            uid: "0".repeat(64),
+            endpoint: "/api/login",
+        };
+
+        const res = await fetch("https://pancham1305-proxy.deno.dev/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then((d) => d.json());
+        if (res.status === 200) {
+            localStorage.setItem("user", JSON.stringify(res.data));
+            window.location.href = "./services.html";
+        } else {
+            guestbtn.innerHTML = temp;
+            const error = document.getElementById("error");
+            error.style.height = "";
+            error.style.top = "";
+            error.innerHTML += `<div class='etext'>${res.message}</div>`;
+        }
+    });
 });
 
 document.addEventListener("click", (e) => {
-  if (e.target.id !== "closeerrorbtn") return;
-  const error = document.getElementById("error");
-  error.innerHTML = '  <div class="closebtn" id="closeerrorbtn">&times;</div>';
-  error.style.height = "0px";
-  error.style.top = "-30px";
+    if (e.target.id !== "closeerrorbtn") return;
+    const error = document.getElementById("error");
+    error.innerHTML =
+        '  <div class="closebtn" id="closeerrorbtn">&times;</div>';
+    error.style.height = "0px";
+    error.style.top = "-30px";
 });
 
 function sendError(msg) {
-  const error = document.getElementById("error");
-  error.style.height = "";
-  error.style.top = "";
-  error.innerHTML += `<div class='etext'>${msg}</div>`;
+    const error = document.getElementById("error");
+    error.style.height = "";
+    error.style.top = "";
+    error.innerHTML += `<div class='etext'>${msg}</div>`;
 }
--- public/js/medhis.js
+++ public/js/medhis.js
@@ -8,62 +8,62 @@ const loader = document.querySelector(".loaderContainer");
 const address = document.querySelector("#address");
 const body = document.querySelector("body");
 testbtn?.addEventListener("click", async (e) => {
-  testb.classList.remove("hide");
-  scanb.classList.add("hide");
-  checkupb.classList.add("hide");
+    testb.classList.remove("hide");
+    scanb.classList.add("hide");
+    checkupb.classList.add("hide");
 });
 
 scanbtn?.addEventListener("click", async (e) => {
-  scanb.classList.remove("hide");
-  testb.classList.add("hide");
-  checkupb.classList.add("hide");
+    scanb.classList.remove("hide");
+    testb.classList.add("hide");
+    checkupb.classList.add("hide");
 });
 
 checkupbtn?.addEventListener("click", async (e) => {
-  testb.classList.add("hide");
-  checkupb.classList.remove("hide");
-  scanb.classList.add("hide");
+    testb.classList.add("hide");
+    checkupb.classList.remove("hide");
+    scanb.classList.add("hide");
 });
 
 // For Hospital Data
 const getData = async () => {
-  const a = new URLSearchParams(window.location.search);
-  const [b, hospitalinfo] = a.get("id").split(":");
+    const a = new URLSearchParams(window.location.search);
+    const [b, hospitalinfo] = a.get("id").split(":");
 
-  // localStorage.removeItem("hospitalinfo");
-  loader?.classList.remove("hide");
-  body?.classList.add("hide");
-  const data = await fetch(
-    "https://hnc-backend.pancham1305.repl.co/api/hospital",
-    {
-      method: "POST",
-      body: JSON.stringify({ hospitalinfo, b }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  ).then((r) => r.json());
-  const arr = data.split(":");
-  console.log(arr);
-  showdata(arr);
+    // localStorage.removeItem("hospitalinfo");
+    loader?.classList.remove("hide");
+    body?.classList.add("hide");
+    const data = await fetch(
+        "https://pancham1305-proxy.deno.dev/",
+        {
+            method: "POST",
+            body: JSON.stringify({ hospitalinfo, b, endpoint: "/api/hospital" }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        },
+    ).then((r) => r.json());
+    const arr = data.split(":");
+    console.log(arr);
+    showdata(arr);
 
-  loader?.classList.add("hide");
-  body?.classList.remove("hide");
+    loader?.classList.add("hide");
+    body?.classList.remove("hide");
 };
 getData();
 const showdata = async (arr) => {
-  const name = document.getElementById("name");
-  name.innerHTML = arr[0].split(",")[0];
-  address.innerHTML = arr[5];
-  const rating = document.getElementById("rating");
-  rating.innerHTML = `Rating: ${arr[3]}`;
+    const name = document.getElementById("name");
+    name.innerHTML = arr[0].split(",")[0];
+    address.innerHTML = arr[5];
+    const rating = document.getElementById("rating");
+    rating.innerHTML = `Rating: ${arr[3]}`;
 };
 
 const login = document.getElementById("login");
 
 const user = JSON.parse(localStorage.getItem("user"));
 if (user) {
-  login.innerHTML = `${user.username}
+    login.innerHTML = `${user.username}
             <span class="material-symbols-outlined" id="loginicon">
               login
             </span>`;
@@ -72,8 +72,8 @@ if (user) {
 const viewmap = document.getElementById("viewmap");
 
 viewmap?.addEventListener("click", async (e) => {
-  const id = new URLSearchParams(window.location.search).get("id");
-  window.location.href = `./map.html?id=${id}`;
+    const id = new URLSearchParams(window.location.search).get("id");
+    window.location.href = `./map.html?id=${id}`;
 });
 
 const Name = document.getElementById("username");
@@ -90,26 +90,33 @@ phone.value = user?.phone;
 
 type.innerHTML = `
 ${Object.keys(list)
-  .map(
-    (x) => `<option value="${x}">${x[0].toUpperCase() + x.slice(1)}</option>`
-  )
-  .join("\n")}
+    .map(
+        (x) =>
+            `<option value="${x}">${x[0].toUpperCase() + x.slice(1)}</option>`,
+    )
+    .join("\n")}
 `;
 
 typevalue.innerHTML = `
   ${list["test"]
-    .map(
-      (x) => `<option value="${x}">${x[0].toUpperCase() + x.slice(1)}</option>`
-    )
-    .join("\n")}
+      .map(
+          (x) =>
+              `<option value="${x}">${
+                  x[0].toUpperCase() + x.slice(1)
+              }</option>`,
+      )
+      .join("\n")}
   `;
 
 type?.addEventListener("change", async (e) => {
-  typevalue.innerHTML = `
+    typevalue.innerHTML = `
   ${list[e.target.value]
-    .map(
-      (x) => `<option value="${x}">${x[0].toUpperCase() + x.slice(1)}</option>`
-    )
-    .join("\n")}
+      .map(
+          (x) =>
+              `<option value="${x}">${
+                  x[0].toUpperCase() + x.slice(1)
+              }</option>`,
+      )
+      .join("\n")}
   `;
 });
