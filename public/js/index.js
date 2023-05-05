ed78aa402077155ee282c7fb21b1e4df996dbbc1
Pancham Mehta <pancham1305@gmail.com>
2023-05-06 00:50:32 +0530

    see what happens

--- public/js/index.js
+++ public/js/index.js
@@ -71,3 +71,10 @@ if (user) {
               login
             </span>`;
 }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("../sw.js").then(() => {
+      console.log("Service Worker Registered");
+    });
+  });
+}
