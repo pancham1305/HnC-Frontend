e515ec50baf1c5e9642ba86fc162e1ecbc01049b
Dhruv-2134 <dhruvpersonal1@gmail.com>
2023-05-09 15:21:06 +0530

    Fixed some minor changes in Emergency Page Pop Up

--- public/js/emergencybtnpopup.js
+++ public/js/emergencybtnpopup.js
@@ -29,12 +29,12 @@ btn4.addEventListener("click", async (e) => {
 });
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
